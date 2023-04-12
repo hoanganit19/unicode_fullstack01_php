e0458bb5e90a5dfcf99ed4a6cf7f6ff040be0b29
Hoang An <contact@unicode.vn>
2023-04-12 15:59:42 +0700

    Update code

--- regex/ex02.php
+++ regex/ex02.php
@@ -4,7 +4,7 @@ $content = 'xin https://unicode.vn chao toi hoangan@gmail.com la Ta Hoang https:
 
 //$pattern = '/([a-z]+[a-z-_\.0-9]{2,})@([a-z]+[a-z\.-_0-9]*\.[a-z]{2,})/';
 
-$pattern = '/(?:https|http):\/\/(?:www\.|)[a-z]+\.([a-z]{2,})/';
+$pattern = '/((?:https|http):\/\/(?:www\.|)[a-z]+\.[a-z]{2,}(?:\/.+|))/';
 
 preg_match_all($pattern, $content, $matches);
 
